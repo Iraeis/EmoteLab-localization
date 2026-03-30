d847f19c3a82dcc5b3d2e97bb7d938d9afae2db5
Iraeis <iraes.xai@gmail.com>
2026-03-30 00:51:27 -0600

    update files to new string table, names

--- src/stitch.py
+++ src/stitch.py
@@ -3,18 +3,31 @@ import os
 
 from translate import lang_dir_walk, lang_dirs
 
-dct_collages = {}
-
 def proc_collage_file(file_path, langs):
     file_name = file_path.split(os.sep)[-1]
     directory = os.sep.join(file_path.split(os.sep)[:-1]) + os.sep
-    df = pd.read_csv(file_path).loc[:, ['Key', 'en']]
+    right_key = 'Key'
+
+    if file_name in ['Main Menu String Table.csv']:
+        left_key = 'Key'
+    elif file_name.split('.')[0] in ['CoffeeBean']:
+        left_key = 'DisplayName'
+    else:
+        raise Exception(f'{__file__}: unrecognized file name pattern')
+
+    df = pd.read_csv(file_path)
+    df = df.loc[:, :'en']  # keep all columns left of and including 'en'
 
     for lang in langs:
         lang_fname = file_name[:-4] + f'_{lang}.csv'
         lang_dir = directory.replace(f'{os.sep}_collage{os.sep}', f'{os.sep}{lang}{os.sep}')
-        df_lang = pd.read_csv(f'{lang_dir}{os.sep}{lang_fname}').loc[:, ['Key', lang]]
-        df = pd.merge(df, df_lang, on='Key', how='outer')
+        df_lang = pd.read_csv(f'{lang_dir}{os.sep}{lang_fname}').loc[:, [right_key, lang]]
+
+        if left_key != right_key:
+            df = pd.merge(df, df_lang, left_on=left_key, right_on=right_key, how='left').drop(columns=[right_key])
+        else:
+            df = pd.merge(df, df_lang, on=left_key, how='left')
+
 
     df.to_csv(file_path, index=False)
 
