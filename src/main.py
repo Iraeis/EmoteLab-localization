88fdb582e9918b185e6113c137966f8df5a3d733
Iraeis <iraes.xai@gmail.com>
2026-04-12 20:34:14 -0600

    prog

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import os
 import pandas as pd
+from deep_translator import GoogleTranslator
 
 """
 input:
 collated csv files (row reference)
 ordered dict langlist
 dct = {
-    lang: translation src,
+    lang: translation src,  - translation source not working right now, just sources english
     ...
 }
 
@@ -29,11 +30,6 @@ dct_langs = {
     'ru': 'en',
 }
 
-class LangDir:
-    def __init__(self):
-        # load and store a language directory
-        raise NotImplementedError
-
 class CSVFile:
     def __init__(self, filepath):
         self.key = 'Key'  # join key
@@ -43,55 +39,165 @@ class CSVFile:
     def __repr__(self):
         return f'"{self.filepath.split(os.sep)[-1]}" on key "{self.key}"'
 
-    def write(self):
-        self.df.to_csv(self.filepath)
+    def write(self, path=None):
+        if path is None:
+            path = self.filepath
+        self.df.to_csv(path, index=False)
 
 class LanguageCSVFile(CSVFile):
-    def update(self):
+    def __init__(self, filepath, lang):
+        super().__init__(filepath)
+        self.lang = lang
+
+    def update(self, ref: CSVFile):
         # put all rows from reference into csv
-        raise NotImplementedError
+        df = pd.merge(ref.df, self.df, how='left', left_on=ref.key, right_on=self.key, suffixes=('', '_y'))
+        df = df[self.df.columns]
+        df['reviewed'] = df['reviewed'].fillna(False)
+        df[self.lang] = df[self.lang].fillna("")
+        self.df = df
 
     def translate(self):
         # machine translate empty
-        raise NotImplementedError
+        src_lang = 'en'
 
-class Collator:
-    def __init__(self, dct_langs, collage_path):
-        # load references (key, en) from collage files
-        self.dct_langs = dct_langs
-        csvs = self.recursive_csvs(collage_path)
+        translate_msk = (self.df[self.lang] == "") & (~self.df["reviewed"].astype(bool))
+        translate_msk &= self.df[src_lang] != ""
+        if translate_msk.any():
+            if self.lang == 'zh-Hant':
+                translation_target = 'zh-TW'
+            elif self.lang == 'zh-Hans':
+                translation_target = 'zh-CN'
+            else:
+                translation_target = self.lang
+
+            translator = GoogleTranslator(source=src_lang, target=translation_target)
+
+            self.df.loc[translate_msk, self.lang] = self.df[translate_msk].apply(lambda row: translator.translate(row[src_lang]), axis="columns")
+
+    def summary(self):
+        return self.df.loc[:, 'reviewed'].sum(), len(self.df)
+
+
+class CSVDir:
+    def __init__(self, dirpath):
+        self.files = [CSVFile(filepath) for filepath in recursive_csvs(dirpath)]
+
+    def dirpath(self, lang):
+        return f'..{os.sep}{lang}'
+
+class RefDir(CSVDir):
+    def __init__(self):
+        super().__init__(self.dirpath('_collage'))
         """
         current patterns: 
         Key, en                         (Main Menu String Table)
         *(wildcard), DisplayName, en    (other; DisplayName is join key)
         """
-        self.dct_refs = {filepath.split(os.sep)[-1]: CSVFile(filepath) for filepath in csvs}
-        for k, v in self.dct_refs.items():
-            col_idx = v.df.columns.get_loc('en')
-            self.dct_refs[k].df = v.df.iloc[:, :col_idx]
-            if k == "Main Menu String Table.csv":
-                self.dct_refs[k].key = "Key"
+        for f in self.files:
+            col_idx = f.df.columns.get_loc('en') + 1
+            f.df = f.df.iloc[:, :col_idx]
+            if f.filepath.split(os.sep)[-1] == "Main Menu String Table.csv":
+                f.key = "Key"
             else:
-                self.dct_refs[k].key = "DisplayName"
+                f.key = "DisplayName"
+        self.files = {f.filepath: f for f in self.files}
+
+    def lang_filepaths(self, lang):
+        # returns the expected file paths corresponding to a language
+        # append _{lang} to file names
+        # adjust path
+        lang_paths = [None] * len(self.files)
+        for i, f in enumerate(self.files.values()):
+            altpath = self.dirpath(lang) + f.filepath[len(self.dirpath('_collage')):]
+            altpath = altpath.split('.csv')[0] + f'_{lang}.csv'
+            lang_paths[i] = (f.filepath, altpath)
+        return lang_paths
+
+
+class LangDir(CSVDir):
+    def __init__(self, lang):
+        self.files = [LanguageCSVFile(filepath, lang) for filepath in recursive_csvs(super().dirpath(lang))]
         """
-        self.dct_langs:     langs: machine translation src
-        self.dct_refs:      filename: CSVFile
+        (cols: Key, en, {lang}, reviewed)
         """
-        # load language files
+        for f in self.files:
+            f.df = f.df.loc[:, ['Key', 'en', f'{lang}', 'reviewed']]
+        self.files = {f.filepath: f for f in self.files}
+        self.lang = lang
 
-    def recursive_csvs(self, root):
-        lst = []
-        for fileroot, _, files in os.walk(root):
-            lst += [f"{fileroot}{os.sep}{file}" for file in files if file.endswith('.csv')]
-        return lst
+    def update(self, ref: RefDir):
+        """
+        look for dir/file
+        if not exist: create
+        if exist: match rows
+        translate
+        """
+        # loop through reference files
+        for (reffp, langfp) in ref.lang_filepaths(self.lang):
+            if langfp not in self.files.keys():
+                # not exists - create
+                ref.files[reffp].write(langfp)
+                f = LanguageCSVFile(langfp, self.lang)  # from reference
+                f.df[self.lang] = None
+                f.df['reviewed'] = False
+                self.files[langfp] = f
+            else:
+                self.files[langfp].update(ref.files[reffp])
+            self.files[langfp].translate()
+            self.files[langfp].write()
 
-    def create_lang_dir(self):
-        # create a language directory - copy from another dir
-        # rename lang, clear translations, set reviewd col to False
-        raise NotImplementedError
+    def summary(self):
+        reviewed, total = 0, 0
+        for f in self.files.values():
+            a, b = f.summary()
+            reviewed += a
+            total += b
+        print(f"{self.lang:8}: {reviewed/total:.2%}")
+        return reviewed, total
 
-    def collate(self):
-        # build collage from langauge dirs
-        raise NotImplementedError
+def recursive_csvs(root):
+    lst = []
+    for fileroot, _, files in os.walk(root):
+        lst += [f"{fileroot}{os.sep}{file}" for file in files if file.endswith('.csv')]
+    return lst
 
-Collator(dct_langs, f"..{os.sep}_collage")
+class Collator:
+    def __init__(self, dct_langs, collage_path):
+        # load references (key, en) from collage files
+        self.dct_langs = dct_langs
+        self.refdir = RefDir()
+
+    def update_collate(self):
+        # foreach
+        """
+        update each language
+        """
+        for k, v in self.dct_langs.items():
+            ld = LangDir(k)
+            ld.update(self.refdir)
+            ld.summary()
+            self.collate(ld, k)
+        for f in self.refdir.files.values():
+            f.write()
+
+    def collate(self, ld: LangDir, lang):
+        for (reffp, langfp) in self.refdir.lang_filepaths(lang):
+            if self.refdir.files[reffp].key == ld.files[langfp].key:
+                self.refdir.files[reffp].df = pd.merge(
+                    self.refdir.files[reffp].df,
+                    ld.files[langfp].df.loc[:, [ld.files[langfp].key, lang]],
+                    how='left',
+                    on=ld.files[langfp].key
+                )
+            else:
+                self.refdir.files[reffp].df = pd.merge(
+                    self.refdir.files[reffp].df,
+                    ld.files[langfp].df.loc[:, [ld.files[langfp].key, lang]],
+                    how='left',
+                    left_on=self.refdir.files[reffp].key,
+                    right_on=ld.files[langfp].key
+                ).drop(columns=ld.files[langfp].key)
+
+c = Collator(dct_langs, f"..{os.sep}_collage")
+c.update_collate()
